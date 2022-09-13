1a09fdbc156c237f592f9edef4a8138160307fe8
SinanIT <sinandegerli@gmail.com>
2022-09-13 09:00:30 -0400

    statging schema created

--- target/run/DBT/models/example/my_second_dbt_model.sql
+++ target/run/DBT/models/example/my_second_dbt_model.sql
@@ -1,8 +1,8 @@
 
-  create or replace  view MY_FIRST_DB.PUBLIC.my_second_dbt_model  as (
+  create or replace  view MY_FIRST_DB.staging.my_second_dbt_model  as (
     -- Use the `ref` function to select from other models
 
 select *
-from MY_FIRST_DB.PUBLIC.my_first_dbt_model
+from MY_FIRST_DB.staging.my_first_dbt_model
 where id = 1
   );
